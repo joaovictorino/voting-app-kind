0f7fe14041bc2b5b46481d99f61ec5fe2ca3c89c
joaovictorino <jhvictorino@gmail.com>
2024-02-11 13:07:15 -0300

    initial commit

--- commands.sh
+++ commands.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Create cluster
 kind create cluster
+
+# Deploy application
+kubectl apply -f k8s/db
 kubectl apply -f k8s
+
+# Access application
+kubectl get svc -n laboratorio
+kubectl get nodes -o wide
